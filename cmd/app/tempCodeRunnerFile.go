3aec7b5c99d9fdd0b64b86163be92518a99a4da7
Reit437 <reitveider@gmail.com>
2025-03-05 22:51:43 +0300

    Last

--- cmd/app/tempCodeRunnerFile.go
+++
@@ -1,5 +0,0 @@
-router.HandleFunc("/api/v1/calculate", ork.CalculateHandler).Methods("POST")
-	router.HandleFunc("/api/v1/expressions/{id}", ork.ExpressionByIDHandler).Methods("GET")
-	router.HandleFunc("/api/v1/expressions", ork.ExpressionsHandler).Methods("GET") // добавляем новый маршрут
-	router.HandleFunc("/internal/task", ork.TaskHandler).Methods("GET")             // добавляем новый маршрут для задачи
-	router.HandleFunc("/internal/task", ork.ResultHandler).Methods("POST")
